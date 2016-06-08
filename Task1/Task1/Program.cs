09000fde0e801e69dd4c29287fa929902b1c3a69
gorilo4ka <gorilo4ka_mk@mail.ru>
2016-06-08 23:35:46 +0300

    commit

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void test(int x = 7, int y = -2)
+        {
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("y = " + y);
+        }
+
+        static void Main(string[] args)
+        {
+            test();
+            Console.WriteLine();
+
+            test(5);
+            Console.WriteLine();
+
+            test(10);
+            Console.WriteLine();
+
+            test(5, 10);
+            Console.WriteLine();
+
+            test(y: 14);
+            Console.WriteLine();
+
+            test(y: 7, x: 4);
+
+            Console.ReadKey();
+        }
+    }
+}
